8a3288f2e28b78566344b74d562e71a9eb943dfe
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-07-03 19:37:03 +0300

    FILE DONE

---
+++ Files/main.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+//#define WRITE_TO_FILE
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef WRITE_TO_FILE
+	cout << "Hello Files";
+	std::ofstream fout;     // Создаем поток
+	fout.open("File.txt", std::ios_base::app);  //Открываем поток
+	fout << "Hello Files" << endl;  //Используем поток
+	fout.close();           //После использования потока, его обязвтельно нужно закрыть
+
+	system("notepad File.txt"); //открыает блокнот вместе с консолью
+
+#endif // WRITE_TO_FILE
+	
+	char sz_filename[FILENAME_MAX] = {};
+	cout << "Введите имя файла: "; cin.getline(sz_filename, FILENAME_MAX);
+
+	if (strstr(sz_filename, ".txt") == NULL)
+	{
+		strcat(sz_filename, ".txt");
+	}
+
+	ifstream fin(sz_filename);
+	if (fin.is_open())
+	{
+		const int SIZE = USHRT_MAX;
+		char buffer[SIZE] = {}; // Буфер чтения
+		while (!fin.eof())
+		{
+			//fin >> buffer;
+			fin.getline(buffer, SIZE);//  вывод в консоль с пробелами
+			cout << buffer << endl;
+		}
+	}
+	else
+	{
+		std::cerr << "Error: file not found" << endl;
+	}
+	fin.close();
+	char  sz_command[FILENAME_MAX] = "start notepad ";
+	strcat(sz_command, sz_filename);
+	system(sz_command);
+}
---
+++ PolymorphicAcademy/main.cpp
@@ -0,0 +1,269 @@
+//Inheritance
+#include<iostream>
+#include<string>
+using namespace std;
+
+#define HUMAN_PARAMETERS	const std::string& last_name, const std::string& first_name, unsigned int age
+#define HUMAN_ARGUMENTS		last_name, first_name, age
+class Human
+{
+	std::string last_name;
+	std::string first_name;
+	unsigned int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+	Human(HUMAN_PARAMETERS)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	virtual ~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+	virtual void info()const
+	{
+		cout << last_name << " " << first_name << " " << age << " years\n";
+	}
+	virtual std::ostream& print(std::ostream& os=cout)const
+	{
+		return os << last_name << " " << first_name << " " << age;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const Human& obj)
+{
+	return obj.print(os);
+}
+
+#define STUDENT_PARAMETERS	const std::string& speciality, const std::string& group, unsigned int year, float rating, float attendance
+#define STUDENT_ARGUMENTS	speciality, group, year, rating, attendance
+class Student :public Human
+{
+	std::string specialty;
+	std::string group;
+	unsigned int year;
+	float rating;
+	float attendance;
+public:
+	const std::string& get_specialty()const
+	{
+		return specialty;
+	}
+	const std::string& get_group()const
+	{
+		return group;
+	}
+	unsigned int get_year()const
+	{
+		return year;
+	}
+	float get_rating()const
+	{
+		return rating;
+	}
+	float get_attendance()const
+	{
+		return attendance;
+	}
+
+	void set_specialty(const std::string& specialty)
+	{
+		this->specialty = specialty;
+	}
+	void set_group(const std::string& group)
+	{
+		this->group = group;
+	}
+	void set_year(unsigned int year)
+	{
+		this->year = year;
+	}
+	void set_rating(float rating)
+	{
+		this->rating = rating;
+	}
+	void set_attendance(float attendance)
+	{
+		this->attendance = attendance;
+	}
+	Student(HUMAN_PARAMETERS, STUDENT_PARAMETERS) :Human(HUMAN_ARGUMENTS)
+	{
+		set_specialty(specialty);
+		set_group(group);
+		set_year(year);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SCostructor:\t" << this << endl;
+	}
+	~Student()
+	{
+		cout << "SDestructor:\t" << this << endl;
+	}
+	void info()const
+	{
+		Human::info();
+		cout << specialty << " " << group << " " << year << " " << rating << " " << attendance << endl;
+	}
+	std::ostream& print(std::ostream& os)const
+	{
+		return Human::print(os) << " "
+			<< specialty << " "
+			<< group << " "
+			<< year << " "
+			<< rating << " "
+			<< attendance;
+	}
+};
+
+#define TEACHER_PARAMETERS	const std::string& specialty, unsigned int experience
+#define TEACHER_ARGUMENTS	specialty, experience
+class Teacher :public Human
+{
+	std::string specialty;
+	unsigned int experience;
+public:
+	const std::string& get_specialty()const
+	{
+		return specialty;
+	}
+	unsigned int get_experience()const
+	{
+		return experience;
+	}
+	void set_specialty(const std::string& specialty)
+	{
+		this->specialty = specialty;
+	}
+	void set_experience(unsigned int experience)
+	{
+		this->experience = experience;
+	}
+	Teacher(HUMAN_PARAMETERS, TEACHER_PARAMETERS) :Human(HUMAN_ARGUMENTS)
+	{
+		set_specialty(specialty);
+		set_experience(experience);
+		cout << "TConstructor:\t" << this << endl;
+	}
+	~Teacher()
+	{
+		cout << "TDestructor:\t" << this << endl;
+	}
+	void info()const
+	{
+		Human::info();
+		cout << specialty << " " << experience << endl;
+	}
+	std::ostream& print(std::ostream& os)const
+	{
+		return Human::print(os) << " " << specialty << " " << experience;
+	}
+};
+
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+	Graduate(HUMAN_PARAMETERS, STUDENT_PARAMETERS, const std::string& subject)
+		:Student(HUMAN_ARGUMENTS, STUDENT_ARGUMENTS)
+	{
+		set_subject(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	void info()const
+	{
+		Student::info();
+		cout << " " << subject << endl;
+	}
+	std::ostream& print(std::ostream& os)const
+	{
+		return Student::print(os) << " " << subject;
+	}
+};
+
+//#define INHERITANCE_CHECK
+
+void main()
+{
+	setlocale(LC_ALL, "");
+#ifdef INHERITANCE_CHECK
+	Human h("Montana", "Antonio", 25);
+	h.info();
+	cout << "\n---------------------------------------------------\n";
+
+	Student jessie("Pinkman", "Jessie", 25, "Chemistry", "WW_220", 1, 70, 90);
+	jessie.info();
+	cout << "\n---------------------------------------------------\n";
+
+	Teacher walter("White", "Walter", 50, "Chemistry", 25);
+	walter.info();
+	cout << "\n---------------------------------------------------\n";
+
+	Graduate hank("Schrader", "Hank", 40, "Criminalistic", "WW_220", 5, 89, 80, "How to catch Heisenberg");
+	hank.info();
+#endif // INHERITANCE_CHECK
+
+	//Polymorphism (многоформенность: poly - много, morphis - форма).
+	//1. ”казатели на базовый класс - Generalisation;	
+	//2. ¬иртуальные метода - Specialisation;
+
+	//Generalisation:
+	Human* group[] =
+	{
+		//Upcast - преобразование к базовому типу:
+		new Student("Pinkman", "Jessie", 25, "Chemistry", "WW_220", 1, 70, 90),
+		new Teacher("White", "Walter", 50, "Chemistry", 25),
+		new Graduate("Schrader", "Hank", 40, "Criminalistic", "WW_220", 5, 89, 80, "How to catch Heisenberg"),
+		new Student("Vercetti", "Tomas", 30, "Criminal", "Vice", 2, 95, 98)
+	};
+	cout << "\n---------------------------------------------------\n";
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		//group[i]->info();
+		//cout << *group[i] << endl;
+		group[i]->print();
+		cout << "\n---------------------------------------------------\n";
+	}
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
+
+}
